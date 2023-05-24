fa4a0718f53ad50f20b86688c95c36fef27255a5
macandrewm <134352378+macandrewm@users.noreply.github.com>
2023-05-23 23:10:05 -0700

    Update site

--- about/_module.js
+++ about/_module.js
@@ -3639,14 +3639,9 @@ function create_fragment$4(ctx) {
 					"title": ""
 				},
 				site_nav: [
+					{ "link": { "url": "/", "label": "Home" } },
 					{
-						"link": { "url": "/projectgold", "label": "Home" }
-					},
-					{
-						"link": {
-							"url": "/projectgold/about",
-							"label": "About"
-						}
+						"link": { "url": "/about", "label": "About" }
 					},
 					{
 						"link": { "url": "/contact", "label": "" }
